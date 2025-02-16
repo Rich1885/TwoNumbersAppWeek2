c5b06d5d39ee92a6bf9296f2f1444c67d08fd875
Rich1885 <mecsekirichard@gmail.com>
2025-02-16 11:54:42 +0000

    Initial commit

---
+++ TwoNumbersAppWeek2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwoNumbersAppWeek2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private TwoNumbers twoNumbers;
+
+        private void btnAddNumbers_Click(object sender, EventArgs e)
+        {
+            if(twoNumbers != null)
+            {
+
+                if(!int.TryParse(txtFirstNum.Text, out int addNum1) )
+                {
+                    MessageBox.Show("Invalid input! Using default value 0 for First Number.");
+                }
+                if(!int.TryParse(txtSecondNum.Text, out int addNum2) )
+                {
+                    MessageBox.Show("Invalid input! Using default value 0 for First Number.");
+                }
+
+                twoNumbers.AddNumbers(addNum1, addNum2);
+                MessageBox.Show("Numbers added successfully!");
+
+                txtFirstNum.Text = twoNumbers.ReturnNum1().ToString();
+                txtSecondNum.Text = twoNumbers.ReturnNum1().ToString();
+            }
+        }
+    }
+}
---
+++ TwoNumbersAppWeek2/TwoNumbers.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoNumbersAppWeek2
+{
+    internal class TwoNumbers
+    {
+        private int FirstNum;
+        private int SecondNum;
+
+        public TwoNumbers(int firstNum, int secondNum) {
+
+            FirstNum = firstNum;
+            SecondNum = secondNum;
+
+        }
+
+        public int ReturnNum1()
+        {
+            return FirstNum;
+        }
+
+        public int ReturnNum2()
+        {
+            return SecondNum;
+        }
+
+        public int ReturnLargeNum()
+        {
+            return Math.Max(FirstNum, SecondNum);   
+        }
+
+        public bool ReturnEqualNum() 
+        {
+            return FirstNum == SecondNum;
+            
+        }
+
+        public int GCD()
+        {
+            int a = FirstNum;
+            int b = SecondNum;
+
+            while(a != b)
+            {
+                if(a > b)
+                {
+                    a = a - b;
+                } else
+                {
+                    b = b - a;
+                }
+            }
+            return a;
+        }
+
+        public void AddNumbers(int firstNum, int secondNum)
+        {
+            FirstNum += firstNum;
+            SecondNum += secondNum;
+        }
+
+        public override string ToString()
+        {
+            return $"First Number: {FirstNum}\n" +
+                   $"Second Number: {SecondNum}\n" +
+                   $"Max: {ReturnLargeNum()}\n" +
+                   $"Equal: {ReturnEqualNum()}\n" +
+                   $"GCD: {GCD()}";
+        }
+
+
+    }
+}
